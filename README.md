b00c7c11ccb72f74a219c8ffcdfcade9824f9e18
humanheima <humanheima@gmail.com>
2018-03-01 15:51:11 +0800

    测试差异化打包

--- README.md
+++ README.md
@@ -69,5 +69,7 @@ press esc to remove highlighting.
 
 
 
+
+
  
 
--- app/src/main/java/com/hm/gradledemo/MainActivity.java
+++ app/src/main/java/com/hm/gradledemo/MainActivity.java
@@ -2,12 +2,14 @@ package com.hm.gradledemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.hm.gradledemo.util.Lg;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = getClass().getSimpleName();
+    private TextView tvPackageName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Lg.e(TAG, getString(R.string.build_time));
         Lg.e(TAG, getString(R.string.build_host));
         Lg.e(TAG, getString(R.string.build_revision));
+        tvPackageName = findViewById(R.id.tv_package_name);
+        tvPackageName.setText("packageName:" + getPackageName());
     }
 }
